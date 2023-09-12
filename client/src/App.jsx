506c53c92dbaa1c6570b3658d2c4d3e1d19421e9
ariya das <ariyadas33@gmail.com>
2023-09-12 20:19:40 +0530

    Routes done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,41 @@
+import { Outlet, Navigate, Route, Routes, useLocation, useLoaderData } from "react-router-dom";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import { About, AuthenticationPage, Companies, CompanyProfile, FindJob, JobDetail, UploadJob } from "./pages";
+
+
+function Layout  () {
+  const user =true;
+  const location =useLocation()
+  return user? (
+    <Outlet/>) : (
+    <Navigate to='user-auth' state={{from:location}}replace/>
+  )
+
+}
 
 export default function App() {
   return (
     <main>
-      <Navbar/>
-      <Footer/>
+      <Navbar />
+      <Routes>
+        {/* User acess page  only after authenticated  */}
+        <Route element={<Layout/>}>
+        <Route path='/' element={<Navigate to='/find-jobs' replace={true}/>}/>
+        <Route path='/find-jobs' element={<FindJob/>}/>
+        <Route path='/companies' element={<Companies/>}/>
+        <Route path ={user?.user?.accountType ==="seeker" ? "/user-profile" : "/user-profile/:id"} element={<UserProfile/>}/>
+        <Route path={"/company-profile"} element={<CompanyProfile/>}/>
+        <Route path={"/company-profile/:id"} element={<CompanyProfile/>}/>
+        <Route path={"/upload-job"} element={<UploadJob/>}/>
+        <Route path={"/job-detail/:id"} element={<JobDetail/>}/>
+        </Route>
+        <Route path="/about-us" element={<About/>}/>
+        <Route path="/user-auth" element={<AuthenticationPage/>}/>
+
+
+      </Routes>
+      {user && <Footer />}
     </main>
-  )
+  );
 }
---
+++ client/src/pages/index.jsx
@@ -0,0 +1,10 @@
+import About from "./About"
+import AuthenticationPage from "./AuthenticationPage"
+import Companies from "./Companies"
+import CompanyProfile from "./CompanyProfile"
+import FindJob from "./FindJob"
+import JobDetail from "./JobDetail"
+import UploadJob from "./UploadJob"
+import UserProfile from "./UserProfile"
+
+export {FindJob,AuthenticationPage,About,Companies,CompanyProfile,JobDetail,UploadJob, UserProfile}
