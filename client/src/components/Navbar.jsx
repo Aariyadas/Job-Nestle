7ceb1139238cef9e9557f31e26889663a17a56cb
ariya das <ariyadas33@gmail.com>
2023-09-18 10:23:14 +0530

    Nav bar  completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,15 +1,104 @@
-
+import React, { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
 import { Link } from "react-router-dom";
+import { AiOutlineLogout } from "react-icons/ai";
+import { CgProfile } from "react-icons/cg";
+import { BiChevronDown } from "react-icons/bi";
 import CustomButton from "./CustomButton";
-import UserMenuList from "./UserMenuList";
-import { useState } from "react";
 import { users } from "../utils/data";
 
+const UserMenuList=({ user }) =>{
+  const handleLogout = () => {
+   
+  };
 
+  return (
+    <div>
+      <Menu as='div' className='inline-block text-left'>
+        <div className='flex'>
+          <Menu.Button className='inline-flex gap-2 w-full rounded-md bg-white md:px-4 py-2 text-sm font-medium text-slate-700 hover:bg-opacity-20 '>
+            <div className='leading[80px] flex flex-col items-start'>
+              <p className='text-sm font-semibold '>
+                {user?.firstName ?? user?.name}
+              </p>
+              <span className='text-sm text-blue-600 '>
+                {user?.jobTitle ?? user?.email}
+              </span>
+            </div>
 
+            <img
+              src={user?.profileUrl}
+              alt='user profile'
+              className='w-10 h-10 rounded-full object-cover '
+            />
+            <BiChevronDown
+              className='h-8 w-8 text-slate-600'
+              aria-hidden='true'
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter='transition ease-out duration-100'
+          enterFrom='transform opacity-0 scale-95'
+          enterTo='transform opacity-100 scale-100'
+          leave='transition ease-in duration-75'
+          leaveFrom='transform opacity-100 scale-100'
+          leaveTo='transform opacity-0 scale-95'
+        >
+          <Menu.Items className='absolute z-50 right-2 mt-2 w-56 origin-top-right divide-y dividfe-gray-100 rounded-md bg-white shadow-lg focus:outline-none '>
+            <div className='p-1 '>
+              <Menu.Item>
+                {({ active }) => (
+                  <Link
+                    to={`${
+                      user?.accountType ? "user-profile" : "company-profile"
+                    }`}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md p-2 text-sm`}
+                    onClick={onClick}
+                  >
+                    <CgProfile
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                      aria-hidden='true'
+                    />
+                    {user?.accountType ? "User Profile" : "Company Profile"}
+                  </Link>
+                )}
+              </Menu.Item>
+
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    onClick={() => handleLogout()}
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "text-gray-900"
+                    } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+                  >
+                    <AiOutlineLogout
+                      className={`${
+                        active ? "text-white" : "text-gray-600"
+                      } mr-2 h-5 w-5  `}
+                      aria-hidden='true'
+                    />
+                    Log Out
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+}
 
 const Navbar = () => {
-  const user=users[1]
+  const user=users[0]
 
   const [isOpen, setIsOpen] = useState(false);
   console.log(user)
@@ -27,16 +116,16 @@ const Navbar = () => {
           </div>
           <ul className="hidden lg:flex gap-10 text-base">
             <li>
-              <Link to="/">Find Job</Link>
+              <Link to="/" onClick={handleCloseNav}>Find Job</Link>
             </li>
             <li>
-              <Link to="/">Companies</Link>
+              <Link to="/" onClick={handleCloseNav}>Companies</Link>
             </li>
             <li>
-              <Link to="/">Upload Job</Link>
+              <Link to="/" onClick={handleCloseNav}>Upload Job</Link>
             </li>
             <li>
-              <Link to="/">About Us</Link>
+              <Link to="/" onClick={handleCloseNav}>About Us</Link>
             </li>
           </ul>
           <div className=" lg:block">
@@ -49,7 +138,7 @@ const Navbar = () => {
               </Link>
             ) : (
              <div>
-              <UserMenuList user={user}/>
+              <UserMenuList user={user} onClick={handleCloseNav}/>
              </div>
             )}
           </div>
--- client/src/utils/data.js
+++ client/src/utils/data.js
@@ -396,7 +396,7 @@
         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       profileUrl: "https://img.freepik.com/premium-vector/red-youtube-logo-social-media-logo_197792-1803.jpg",
       jobPosts: ["1", "2"],
-      token: "gjhsdgsjgdjh",
+      token: "Google",
     },
     {
       firstName: "CodeWaver",
@@ -408,6 +408,6 @@
       profileUrl: "https://img.freepik.com/premium-vector/red-youtube-logo-social-media-logo_197792-1803.jpg",
       accountType: "seeker",
       cvUrl: "",
-      token: "gjhsdgsjgdjh",
+      token: "Coders",
     },
   ];
